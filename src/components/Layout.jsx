76b3d7b0aa7e657b662edd50ffae6e13d27a9ea9
Cahangir_Asgerov <esgerovcahangir12@gmail.com>
2024-09-11 03:17:46 +0400

    Header and footer will be added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import "../assets/css/components/layout.scss";
+
+import { routes } from "../routes";
 
 const Layout = () => {
+  const location = useLocation();
+  const locationPathname = location.pathname;
+  const hideHeaderAndFooter =
+    routes.find((route) => route.path === locationPathname) &&
+    locationPathname !== "*";
   return (
     <div>
-      <Outlet />
+      {hideHeaderAndFooter && <Header />}
+      <div className="container sectionStyles">
+        {hideHeaderAndFooter && <Navbar />}
+        <Outlet />
+      </div>
+      {hideHeaderAndFooter && <Footer />}
     </div>
   );
 };
